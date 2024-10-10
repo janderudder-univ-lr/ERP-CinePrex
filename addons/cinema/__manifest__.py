55bc18e357cba1cdcb6eb1612e7c778954c8f66c
TotoDev <totooodev@gmail.com>
2024-10-10 12:41:55 +0200

    added cinema

---
+++ addons/cinema/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Cinema',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'TotoEnF5',
+    'category': 'Uncategorized',
+    'description': 'A cinema',
+
+    'installable': True,
+    'application': True,
+
+    'data': [
+        'security/ir.model.access.csv'
+    ]
+}
